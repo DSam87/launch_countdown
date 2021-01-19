74178d2e45202cd11efa298d851c4155fb09458e
Sam <deleonsam87@gmail.com>
2021-01-18 18:14:31 -0600

    bug fixed, each loop needed its own para created, then appended to div output

--- script.js
+++ script.js
@@ -8,13 +8,16 @@ let i = 10;
 while(i>=0){
 
     if(i === 10){
+        const para = document.createElement('p');
         para.textContent = 'Countdown 10';
         output.appendChild(para);
     }else if(i !== 0){
+        const para = document.createElement('p');
         para.textContent = `${i}`;
         output.appendChild(para);
         console.log(i);
     }else if(i === 0){
+        const para = document.createElement('p');
         para.textContent = `Blast off!`;
         output.appendChild(para);
     }
